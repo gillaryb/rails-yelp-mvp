81bdc051a8f8ef9519bde1411d3df716b18fd7e0
Gilary Bacnis <gilarybacnis@gmail.com>
2023-02-15 15:48:37 +0900

    created restaurants

--- Gemfile
+++ Gemfile
@@ -42,8 +42,14 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
+# simple form
+gem "simple_form", github: "heartcombo/simple_form"
+
+# faker
+gem 'faker'
+
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database'
+Restaurant.destroy_all
+puts 'Creating restaurants'
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
+
+puts "Created #{Restaurant.count} restaurants."
